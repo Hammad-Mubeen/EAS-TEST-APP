01e822160e1d5d5d07e8172c9ee45d715a4411f7
Muhammad Hammad Mubeen <twoface03205622887@gmail.com>
2024-08-12 18:57:54 +0500

    UI completed for verification

--- src/pages/verifyDoc.tsx
+++ src/pages/verifyDoc.tsx
@@ -9,7 +9,22 @@ const { createHash } = require('crypto');
 export default function VerifyDoc() {
 	const [file, setFile] = useState<File | null>(null);
 	const [UID, setUID] = useState('');
+	const [docHash, setdocHash] = useState('');
+	const [note, setNote] = useState('');
+	const [showDocStatus, setShowDocStatus] = useState<string>("null");
+	let [color,setTextColor] = useState ('red'); 
 	const picker = useId();
+
+	function toggle(status:string){
+		setShowDocStatus(status);
+		if(status == "true")
+		{
+			setTextColor('green');
+		}
+		else{
+			setTextColor('red');
+		}
+	}
 
 	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const fileData = event.target.files?.[0];
@@ -23,33 +38,65 @@ export default function VerifyDoc() {
 			let hash = createHash('sha256').update(array).digest('hex');
 			hash='0x'+hash;
 			console.log("Doc Hash to verify: ",hash);
-			console.log("Attestation UID: ",UID);
-			var attestatoinData = await accessAttestation(UID);
-			console.log("Attestatoion Data: ",attestatoinData);
+			if(hash == docHash)
+			{
+				console.log("Document Verified Successfully.");
+				toggle("true");
+			}
+			else{
+				console.log("Document not verified.");
+				toggle("false");
+			}
 		  };
 		  reader.readAsArrayBuffer(fileData);
 		}
 	};
 	useEffect(() => {
 		if (localStorage?.getItem("UID")){
-		  setUID(localStorage.getItem("UID") as any);
+		  let UID = localStorage.getItem("UID") as any;
+		  setUID(UID);
+		  console.log("Attestation UID: ",UID);
+		  accessAttestation(UID)
+		  .then((attestatoinData) => {
+			console.log("Attestation Document Hash: ",attestatoinData![0].value.value);
+			console.log("Attestation Note: ",attestatoinData![1].value.value);
+			// @ts-ignore
+			setdocHash(attestatoinData![0].value.value);
+			// @ts-ignore
+			setNote(attestatoinData![1].value.value);
+		  })
+		  .catch(() => {
+			console.log('Error occured when fetching attestation...');
+		  });
 		}
 	}, []);
+
 	return (
 		<div>
 			<div className="flex flex-col  items-center" style={{ margin: 50}}>
 				<Card className="border rounded-md" style={{ width: 650, borderRadius: 10 }}>
 				<CardHeader >
-					<div style={{ marginLeft:20,marginTop:20, fontSize:25,fontWeight:'bold',color:"#3e4c59"}}>
+					<div style={{ marginLeft:20,marginTop:20, fontSize:25,fontWeight:'bold',color:"#000000"}}>
 						<text>OnChain Attestation</text>
 					</div>
 				</CardHeader>
 				<CardBody>
-				<div className=" flex flex-col" style={{ margin:20, fontSize:13,color:"#7b8794"}}>
-						<text style={{ fontWeight:'bold'}}>SCHEMA</text>
+				<div className=" flex flex-col" style={{ marginLeft:20,marginRight:20, marginBottom:20,marginTop:10, fontSize:13}}>
+						<text style={{marginTop:7,fontWeight:'bold',color:"#7b8794"}}>UID:</text>
+						<text style={{ color:"#000000",fontWeight:'bold'}}>{UID}</text>
+						<text style={{  marginTop:7,fontWeight:'bold',color:"#7b8794"}}>SCHEMA:</text>
 						<div className=" flex flex-col border rounded-md" style={{ marginTop:7}}>
 						<text style={{ marginLeft:8,marginTop:8,color:'#19216c',fontWeight:'bold'}}>#15 SIGN DOCUMENT</text>
 						<a href={"https://sepolia.easscan.org/schema/view/" + process.env.NEXT_PUBLIC_SCHEMA_UID} style={{color:'#30377b',marginLeft:8,marginBottom:8}}>{process.env.NEXT_PUBLIC_SCHEMA_UID}</a>
+						</div>
+						<text style={{ fontWeight:'bold',marginTop:7,color:"#7b8794"}}>DECODED DATA:</text>
+						<div className=" flex flex-col border rounded-md" style={{ marginTop:7}}>
+						<text style={{ marginLeft:8,marginTop:8,color:'#19216c',fontWeight:'bold'}}>Hash of Document | BYTES32</text>
+						<text style={{ marginLeft:8,marginBottom:8,color:"#7b8794"}} >{docHash}</text>
+						</div>
+						<div className=" flex flex-col border rounded-md" style={{ marginTop:7}}>
+						<text style={{ marginLeft:8,marginTop:8,color:'#19216c',fontWeight:'bold'}}>Note | STRING</text>
+						<text style={{ marginLeft:8,marginBottom:8,color:"#7b8794"}} >{note}</text>
 						</div>
 						<div className='aspect-square bg-gray-200 border rounded-md' style={{marginTop:20,width:607,height:210}}>
 						<label htmlFor={picker}>
@@ -75,6 +122,10 @@ export default function VerifyDoc() {
 							id={picker}
 							onChange={handleFileChange}
 						/>
+						</div>
+						<div className="flex flex-row items-center justify-center" style={{ marginTop:7,fontSize:15}}>
+						<text style={{ marginLeft:8,marginTop:8,color:"#7b8794",fontWeight:'bold'}}>Verification Status:</text>
+						<text style={{ marginLeft:8,marginTop:8,color:color,fontWeight:'bold'}}>{showDocStatus}</text>
 						</div>
 					</div>
 				</CardBody>
--- src/scripts/accessAttestation.js
+++ src/scripts/accessAttestation.js
@@ -1,16 +1,23 @@
 require("dotenv").config();
-var { EAS} = require('@ethereum-attestation-service/eas-sdk');
+var {EAS,SchemaEncoder} = require('@ethereum-attestation-service/eas-sdk');
 import provider from '../ethers/ether';
 
 const EASContractAddress = process.env.NEXT_PUBLIC_EAS_CONTRACT_ADDRESS; // Sepolia v0.26
 
-export default async function accessAttestation(attestationUID)
-{
-    const eas = new EAS(EASContractAddress);
-    const signer = await provider.getSigner();
-    eas.connect(signer);
-    const attestation = await eas.getAttestation(attestationUID);
-    return attestation;
+export default async function accessAttestation(attestationId) {
+    try {
+        const signer = await provider.getSigner();
+        const eas = new EAS(EASContractAddress);
+        eas.connect(signer);
+        const attestation = await eas.getAttestation(attestationId);
+        if (!attestation) {
+            throw new Error('Attestation not found');
+        }
+        // Decode the attestation data
+        const schemaEncoder = new SchemaEncoder("bytes32 hashOfDocument,string note");
+        const decodedData = schemaEncoder.decodeData(attestation.data);
+        return decodedData;
+    } catch (error) {
+        console.error('Error decoding attestation:', error);
+    }
 }
-
-
